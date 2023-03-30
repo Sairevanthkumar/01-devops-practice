35758a787143cff875eb0d1895a8c0015574a5d6
Sairevanthkumar <sairevanth8268@gmail.com>
2023-03-30 19:29:53 +0530

    stash apply command practise

--- revanth.java
+++ revanth.java
@@ -1,1 +1,2 @@
 hello i am edit some of the notes in revanth.java file
+hello guys now i am practising the git stash command this git stash command is used to save the temparary data and working tree clean
